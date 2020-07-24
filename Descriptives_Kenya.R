ec5f1a213bd5d5174c4a83d97b154bd5a585b5e2
harriok20 <harriok20@yahoo.com>
2020-07-24 15:37:08 +0000

    More changes

--- Descriptives_Kenya.R
+++ Descriptives_Kenya.R
@@ -1,74 +1,90 @@
-dataset <- read.csv('~/SESYNK LAKEVICTORIA.csv') #load data
+# 1) load data
+dataset <- read.csv('~/SESYNK LAKEVICTORIA.csv', na.strings = c('NA', '')) 
 
-#subset to include only the variables we need
+
+# 2) subset to include only the variables we need
 subset<- cbind(hhid,hv002,hv009,hv024,hv201,hv202,hv204,hv205,
            hv206,hv219,hv220,hv226,hv230a,hv230b,hv232,hv235,hv236,
            hv237a,hv237b,hv237c,hv237d,hv237e,hv237f,hv237g,hv237h,
            hv237i,hv237j,hv237k,hv237x,hv237z,
           hv238,hv270)
 
-#household head age (mean and SD)
+# 3) means and sd for multiple columns
+dataset %>%
+  summarise_at(vars(hv220, hv009), list(mean = mean, sd=sd), na.rm=T)
+
+# 4) means and sd for individual columns:
+
+# 4a) household head age (mean and SD)
 meanage<-mean(hv220, na.rm=TRUE)
 sd(dataset$hv220, na.rm=TRUE)
 
-#household size (mean and SD)
+# 4b)household size (mean and SD)
 meanhhsize<-mean(hv009, na.rm=TRUE)             
 sd(dataset$hv009)
 
-#frequencies & proportions
+# 5) Frequencies & proportions
 gender<-table(dataset$hv219) #frequency
 gender
 prop.table(gender)           #proportion
 
-
-######show to Mary...blanks are still being included in the values#####
-#electricity
-electricity<-table(dataset$hv206) #deleted the na.rm = TRUE as per Mary's advice
+# a.electricity 
+#[useNA=no tells R to not include the NAs]
+electricity<-table(dataset$hv206, useNA='no')   
 electricity
 prop.table(electricity)          
 
-#wealth index
+# b.wealth index
 wealth<-table(dataset$hv270) 
 wealth
 prop.table(wealth)          
 
-#Toilet use
+# c.toilet use
 toilet<-table(dataset$hv225) 
 toilet
 prop.table(toilet) 
 
-#cooking fuel
+# d.cooking fuel
 cook_fuel<-table(dataset$hv226) 
 cook_fuel
 prop.table(cook_fuel) 
 
-#Source of drinking water 
+# e.source of drinking water 
 drinking<-table(dataset$hv201) 
 drinking
 drink<-prop.table(drinking) 
 
-#Bar graphs
+# 6) Bar graphs
 
 library (readr)
 dataset2<- read.csv(file = '~/SESYNK LAKEVICTORIA.csv')
 library(ggplot2)
 
-#####ask Mary how to fix the x-axis values#######
-ggplot(dataset2, aes(hv201)) + 
+# a. Source of drinking water
+# First, lets rename one of the response options for a variable 
+#[Change "river/dam/lake/ponds/stream/canal/irrigation channel" 
+#to "river/lake"]
+
+library(dplyr)
+dataset3 <- dataset %>%
+  mutate(hv201 = gsub('river/dam/lake/ponds/stream/canal/irrigation channel', 
+                      'river/lake', hv201)) 
+
+ggplot(dataset3, aes(hv201)) + 
   geom_bar (aes(y=(..count..)/sum
 (..count..))) + xlab("Source of drinking water")+
-scale_y_continuous (labels=scales::percent, name="percent")
+scale_y_continuous (labels=scales::percent, name="percent") +
   theme (axis.text.x=element_text(angle=45, hjust=1))
 
-#Do you treat your water to make it safe?
-  ggplot(dataset2, aes(hv237)) + 
+# b.do you treat your water to make it safe?
+  ggplot(dataset3, aes(hv237)) + 
     geom_bar (aes(y=(..count..)/sum (..count..))) +
     labs(title='Do you do anything to your water to make it safe for drinking?',
          x= 'Treatment of water for dinking') + 
     scale_y_continuous (labels=scales::percent, name="percent")
   theme (axis.text.x=element_text(angle=45, hjust=1))
   
-#water treatment = boiling
+# c.water treatment = boiling
 ggplot(dataset2, aes(hv237a)) + 
 geom_bar (aes(y=(..count..)/sum (..count..))) +
   labs(title='How do you treat your water to make it safe to drink?',
@@ -76,7 +92,7 @@ geom_bar (aes(y=(..count..)/sum (..count..))) +
 scale_y_continuous (labels=scales::percent, name="percent")
 theme (axis.text.x=element_text(angle=45, hjust=1))
   
-#water treatment = Adding bleach/chlorine
+#d. water treatment = Adding bleach/chlorine
 ggplot(dataset2, aes(hv237b)) + 
   geom_bar (aes(y=(..count..)/sum (..count..))) +
   labs(title='How do you treat your water to make it safe to drink?',
@@ -84,7 +100,7 @@ ggplot(dataset2, aes(hv237b)) +
  scale_y_continuous (labels=scales::percent, name="percent")
 theme (axis.text.x=element_text(angle=45, hjust=1))
 
-#water treatment = Straining through cloth
+# e.water treatment = Straining through cloth
 ggplot(dataset2, aes(hv237c)) + 
   geom_bar (aes(y=(..count..)/sum (..count..))) + 
   labs(title='How do you treat your water to make it safe to drink?',
@@ -92,7 +108,7 @@ ggplot(dataset2, aes(hv237c)) +
   scale_y_continuous (labels=scales::percent, name="percent")
 theme (axis.text.x=element_text(angle=45, hjust=1))
 
-#water treatment = Using water filter
+# f.water treatment = Using water filter
 
 ggplot(dataset2, aes(hv237d)) + 
   geom_bar (aes(y=(..count..)/sum(..count..))) + 
@@ -101,7 +117,7 @@ ggplot(dataset2, aes(hv237d)) +
  scale_y_continuous (labels=scales::percent, name="percent")
 theme (axis.text.x=element_text(angle=45, hjust=1))
 
-#water treatment = Solar disinfection
+# g.water treatment = Solar disinfection
 
 ggplot(dataset2, aes(hv237e)) + 
   geom_bar (aes(y=(..count..)/sum(..count..))) + 
@@ -110,7 +126,7 @@ ggplot(dataset2, aes(hv237e)) +
   scale_y_continuous (labels=scales::percent, name="percent")
 theme (axis.text.x=element_text(angle=45, hjust=1))
 
-#water treatment = stand and settle
+# h.water treatment = stand and settle
 ggplot(dataset2, aes(hv237f)) + 
   geom_bar (aes(y=(..count..)/sum(..count..))) + 
   labs(title='How do you treat your water to make it safe to drink?',
@@ -118,7 +134,7 @@ ggplot(dataset2, aes(hv237f)) +
   scale_y_continuous (labels=scales::percent, name="percent")
 theme (axis.text.x=element_text(angle=45, hjust=1))  
 
-#water treatment = Covering the water container
+# i.water treatment = Covering the water container
 
 ggplot(dataset2, aes(hv237g)) + 
   geom_bar (aes(y=(..count..)/sum(..count..))) + 
@@ -128,4 +144,19 @@ ggplot(dataset2, aes(hv237g)) +
 theme (axis.text.x=element_text(angle=45, hjust=1))
 
 
+
+#######extra coding examples##################
+# get numbers for hv206 where all "no" are excluded
+table(dataset$hv206, exclude='no') # excludes all "no" responses
+
+# get number where only female in hv219
+dataset %>%
+  filter(hv219 == 'female') %>%
+  dplyr::select(hv206)%>%
+  na.omit()%>%
+  group_by(hv206) %>%
+  summarize(n())
+
+dataset %>%
+  distinct(hv206)
 
